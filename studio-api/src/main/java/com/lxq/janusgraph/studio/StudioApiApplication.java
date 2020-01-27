a79650f11d8c35d4e2ced6df1b62108652410bb2
fjlxq1994 <lxq@362805>
2020-01-27 23:21:09 +0800

    init repo

---
+++ studio-api/src/main/java/com/lxq/janusgraph/studio/StudioApiApplication.java
@@ -0,0 +1,13 @@
+package com.lxq.janusgraph.studio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudioApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudioApiApplication.class, args);
+    }
+
+}
